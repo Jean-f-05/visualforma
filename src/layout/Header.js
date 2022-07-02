4a67ceb574f4254c9d9f852c905e3936957fbe28
John Francis <jean_f_05@hotmail.com>
2022-07-02 11:22:28 +0100

    added src subfolders

---
+++ src/layout/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <Header>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      <div></div>
+    </Header>
+  );
+};
+
+export default Header;
